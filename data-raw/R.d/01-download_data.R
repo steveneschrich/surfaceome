43e48eb90772a0e65c31c593225040dca4802def
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2023-04-19 13:06:06 -0400

    Cleaning up downloads

--- data-raw/R.d/01-download_data.R
+++ data-raw/R.d/01-download_data.R
@@ -2,15 +2,21 @@ dir.create(here::here("data-raw/cspa"), showWarnings=FALSE)
 dir.create(here::here("data-raw/surfy"), showWarnings=FALSE)
 
 
-logger::log_info("Downloading S2_File")
+src <- "https://wlab.ethz.ch/cspa/data/S2_File.xlsx"
+dest <- "data-raw/cspa/S2_File.xlsx"
+cli::cli_alert_info("Downloading {src}")
 curl::curl_download(
-  url="https://wlab.ethz.ch/cspa/data/S2_File.xlsx",
-  destfile = here::here("data-raw/cspa/S2_File.xlsx")
+  url=src,
+  destfile = here::here(dest)
 )
+cli::cli_alert_success("Downloaded {dest} ({fs::file_size(here::here(dest))})")
 
-logger::log_info("Downloading S3_surfaceome")
+src <- "http://wlab.ethz.ch/surfaceome/table_S3_surfaceome.xlsx"
+dest <- "data-raw/surfy/table_S3_surfaceome.xlsx"
+cli::cli_alert_info("Downloading {src}")
 curl::curl_download(
-  url="http://wlab.ethz.ch/surfaceome/table_S3_surfaceome.xlsx",
-  destfile = here::here("data-raw/surfy/table_S3_surfaceome.xlsx")
+  url=src,
+  destfile = here::here(dest)
 )
+cli::cli_alert_success("Downloaded {dest} ({fs::file_size(here::here(dest))})")
 
